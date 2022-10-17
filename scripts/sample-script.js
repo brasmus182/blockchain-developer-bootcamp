db11b9841594b1e9bb219d3691d21f973e1beeaa
Ben Rasmussen <ben@Bens-Mac-mini.local>
2022-10-17 14:05:02 -0500

    First Commit of Monday

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -1,11 +1,11 @@
 async function main() {
 
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+ const Token = ethers.getContractFactory("Token");
 
-  await greeter.deployed();
+ const token = Token.deploy();
+ await token.deployed();
+ console.log("Greeter deployed to:", greeter.address);
 
-  console.log("Greeter deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
