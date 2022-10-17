30951c5b83e5af75a5a4c4683d530b6343ab18a8
Ben Rasmussen <ben@Bens-Mac-mini.local>
2022-10-17 17:31:38 -0500

    2nd Monday Commit2nd Monday Commit

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,11 +9,17 @@ contract Token {
 	uint256 public decimals;
 	uint256 public totalSupply;
 
+	//Track Balances
+	mapping(address => uint256) public balanceOf; 
+	//Send tokens
+
+
 	constructor(string memory _name, string memory _symbol, uint256 _decimals, uint256 _totalSupply) {
 		name = _name;
 		symbol = _symbol;
 		decimals = _decimals;
-		totalSupply = _totalSupply;
+		totalSupply = _totalSupply * (10**decimals);
+		balanceOf[msg.sender] = totalSupply;
 	}
 }
 
--- test/Token.js
+++ test/Token.js
@@ -6,19 +6,20 @@ const tokens = (n) => {
 }
 
  describe('Token', async ()=> {
- 	let token; 
-
+ 	let token, accounts, deployer;
 
  	beforeEach( async () => {
  		const Token = await ethers.getContractFactory('Token');
-		token = await Token.deploy('Dapp University', 'DAPP', '18', tokens('1000000'));
-
+		token = await Token.deploy('Dapp University', 'DAPP', '18', '1000000');
+		accounts = await ethers.getSigners();
+		deployer = accounts[0];
  	})
  	describe('Deployment', () => {
  		const name = 'Dapp University';	
  		const symbol = 'DAPP';
  		const decimals = '18';
  		const totalSupply = tokens('1000000');
+
 	 	it('has correct name', async () => {
 	 		expect(await token.name()).to.equal(name);
 	 	})
@@ -27,11 +28,12 @@ const tokens = (n) => {
 	 	})
 	 	it('has correct decimals', async () => {
 	 		expect(await token.decimals()).to.equal(decimals);
-
 	 	})
 	 	it('has correct supply', async () => {
-	 		
 	 		expect(await token.totalSupply()).to.equal(totalSupply);
+	 	})
+	 	it('Assigns total supply to deployer', async () => {
+	 		expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
 	 	})
 	 })
  })
